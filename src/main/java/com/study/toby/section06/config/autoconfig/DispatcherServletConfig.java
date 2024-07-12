08ee17fbec7ef75bc2c369f4720d0907dd86a31c
TunaHG <asdf0185@naver.com>
2024-07-12 21:20:17 +0900

    Section 06 자동 구성 정보 학습

--- src/main/java/com/study/toby/section06/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/com/study/toby/section06/config/autoconfig/DispatcherServletConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+// @Configuration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
