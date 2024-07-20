e0cc9914b71b061d830dc1fdf015e2ae2d059a3c
TunaHG <asdf0185@naver.com>
2024-07-20 15:29:31 +0900

    Section 07-2, 3 `@Conditional` 학습

---
+++ src/test/java/com/study/toby/section07/helloboot/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.study.toby.section07.helloboot;
+
+import com.study.toby.section05.helloboot.HelloController;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class HelloControllerTest {
+    @Test
+    void helloController() {
+        HelloController
+            controller = new HelloController(name -> name);
+
+        String res = controller.hello("Test");
+
+        assertThat(res).isEqualTo("Test");
+    }
+
+    @Test
+    void failHelloController() {
+        HelloController controller = new HelloController(name -> name);
+
+        assertThatThrownBy(() -> controller.hello(null))
+            .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> controller.hello(""))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
