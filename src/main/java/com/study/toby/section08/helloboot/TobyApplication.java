99a785644dcc42df841e448a55c1f0eea335aa37
TunaHG <asdf0185@naver.com>
2024-07-28 15:35:39 +0900

    Section 08-1, 2 Environment 학습

--- src/main/java/com/study/toby/section08/helloboot/TobyApplication.java
+++ src/main/java/com/study/toby/section08/helloboot/TobyApplication.java
@@ -1,10 +1,21 @@
 package com.study.toby.section08.helloboot;
 
 import com.study.toby.section08.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class TobyApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my.name: " + name);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TobyApplication.class, args);
