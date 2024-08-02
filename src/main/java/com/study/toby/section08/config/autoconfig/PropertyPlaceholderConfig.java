84b48599077dd94237292d641c4ce944db6f8646
TunaHG <asdf0185@naver.com>
2024-08-02 19:37:23 +0900

    Section 08 마무리

---
+++ src/main/java/com/study/toby/section08/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,13 @@
+package com.study.toby.section08.config.autoconfig;
+
+import com.study.toby.section08.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
--- src/main/java/com/study/toby/section08/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/study/toby/section08/config/autoconfig/TomcatWebServerConfig.java
@@ -1,21 +1,25 @@
 package com.study.toby.section08.config.autoconfig;
 
 import com.study.toby.section08.config.ConditionalMyOnClass;
+import com.study.toby.section08.config.EnableMyConfigurationProperties;
 import com.study.toby.section08.config.MyAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+
         return factory;
     }
 }
