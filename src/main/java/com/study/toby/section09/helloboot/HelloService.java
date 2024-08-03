708daa82349bcc17298a73dc6e7133909f1671de
TunaHG <asdf0185@naver.com>
2024-08-04 08:07:57 +0900

    Section 09 준비

---
+++ src/main/java/com/study/toby/section09/helloboot/HelloService.java
@@ -0,0 +1,5 @@
+package com.study.toby.section09.helloboot;
+
+public interface HelloService {
+    String sayHello(String name);
+}
---
+++ src/main/java/com/study/toby/section09/helloboot/TobyApplication.java
@@ -0,0 +1,24 @@
+package com.study.toby.section09.helloboot;
+
+import com.study.toby.section09.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@MySpringBootApplication
+public class TobyApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my.name: " + name);
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyApplication.class, args);
+    }
+
+}
