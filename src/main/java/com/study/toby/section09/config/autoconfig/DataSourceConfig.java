049a83d88b92f52ec3d4412383cfaab13157676e
TunaHG <asdf0185@naver.com>
2024-09-09 18:49:56 +0900

    Section 09-3 JdbcTemplate과 트랜잭션 매니저 구성

--- src/main/java/com/study/toby/section09/config/autoconfig/DataSourceConfig.java
+++ src/main/java/com/study/toby/section09/config/autoconfig/DataSourceConfig.java
@@ -5,8 +5,12 @@ import com.study.toby.section09.config.EnableMyConfigurationProperties;
 import com.study.toby.section09.config.MyAutoConfiguration;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.sql.Driver;
 
@@ -15,6 +19,7 @@ import javax.sql.DataSource;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
     @Bean
     @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
@@ -41,5 +46,19 @@ public class DataSourceConfig {
         dataSource.setPassword(properties.getPassword());
 
         return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
     }
 }
---
+++ src/test/java/com/study/toby/section09/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,39 @@
+package com.study.toby.section09.helloboot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@HelloBootTest
+// @Rollback(false)
+public class JdbcTemplateTest {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS member(name varchar(50) PRIMARY KEY, count int)");
+    }
+
+    @Test
+    void insertAndQuery() {
+        jdbcTemplate.update("INSERT INTO member(name, count) VALUES(?, ?)", "Toby", 3);
+        jdbcTemplate.update("INSERT INTO member(name, count) VALUES(?, ?)", "Spring", 1);
+
+        Long count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM member", Long.class);
+        assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    void insertAndQuery2() {
+        jdbcTemplate.update("INSERT INTO member(name, count) VALUES(?, ?)", "Toby", 3);
+        jdbcTemplate.update("INSERT INTO member(name, count) VALUES(?, ?)", "Spring", 1);
+
+        Long count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM member", Long.class);
+        assertThat(count).isEqualTo(2);
+    }
+}
