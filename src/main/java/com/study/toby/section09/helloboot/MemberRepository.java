09083b710fbe545fd76069382501918df320bccb
TunaHG <asdf0185@naver.com>
2025-01-15 18:38:48 +0900

    Section 09-4 Hello 레포지토리

---
+++ src/main/java/com/study/toby/section09/helloboot/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.study.toby.section09.helloboot;
+
+public interface MemberRepository {
+    Member findMember(String name);
+
+    void increaseCount(String name);
+
+    default int countOf(String name) {
+        Member member = findMember(name);
+        return member == null ? 0 : member.getCount();
+    }
+}
